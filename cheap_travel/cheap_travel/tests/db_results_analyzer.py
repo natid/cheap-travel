7802f3e76aeead77470a265e1b834f2a1bb89a45
magenn <magenheim@gmail.com>
2014-07-07 15:08:42 +0300

    improve db_results_analyzer

--- cheap_travel/cheap_travel/tests/db_results_analyzer.py
+++ cheap_travel/cheap_travel/tests/db_results_analyzer.py
@@ -1,6 +1,7 @@
 from db.flights_resp import FlightsRespDAL
 from datetime import timedelta, time, date
 from flights_data.flight_checks import FlightChecker
+import tester
 
 def check_single(origin, dest, depart_date, return_date):
 
@@ -35,26 +36,16 @@ def check_single(origin, dest, depart_date, return_date):
         print "didn't find cheeper price"
     print last_result
 
-flight_checker = FlightChecker()
+if __name__ == "__main__":
+    flight_checker = FlightChecker()
 
-
-
-origin_list = ['LON', 'BER', 'AMS', 'ROM', 'PAR', 'ZRH']
-dest_list = ['MNL', 'BKK', 'HKG', 'BJS']
-
-depart_dates = [date(2014, 11, 02), date(2014, 9, 15), date(2015, 01, 01), date(2014, 07, 02), date(2014, 06, 25)]
-return_dates = [depart_date  + timedelta(days=21) for depart_date in depart_dates]
-return_dates += [depart_date  + timedelta(days=7) for depart_date in depart_dates]
-return_dates += [depart_date  + timedelta(days=80) for depart_date in depart_dates]
-
-
-for dest in dest_list:
-    for origin in origin_list:
-        for depart_date in depart_dates:
-            for return_date in return_dates:
-                if depart_date < return_date :
-                    if origin != dest:
-                        check_single(origin, dest, depart_date, return_date)
+    for dest in tester.dest_list:
+        for origin in tester.origin_list:
+            for depart_date in tester.depart_dates:
+                for return_date in tester.return_dates:
+                    if depart_date < return_date :
+                        if origin != dest:
+                            check_single(origin, dest, depart_date, return_date)
 
 
 
